20aa3d735a77e1ac6341de558ba0956fdfa2f5ff
Mevlut Kama <mtkama02@gmail.com>
2023-02-14 21:05:08 +0300

    api

--- src/test/java/patch_requests/Patch01.java
+++ src/test/java/patch_requests/Patch01.java
@@ -14,6 +14,7 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertEquals;
 
 public class Patch01 extends JsonPlaceHolderBaseUrl {
+
     /*
             Given
                 1) https://jsonplaceholder.typicode.com/todos/198
